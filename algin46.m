5d8ffccb30ab3400c4368da781a45d46164050c0
suminmath <142664958+suminmath@users.noreply.github.com>
2023-08-21 11:01:25 +0800

    algin46.m

--- algin46.m
+++ algin46.m
@@ -9,7 +9,7 @@ zn = 2*x0;
 un = 2*u0;
  while err4 > Eps1
    wn = zn+alphan*(zn-z1n);
-   mn = un+alphan*(un-u1n);%(分块)
+   mn = un+alphan*(un-u1n);%(路脰驴茅)
    xnn = wn-gamma*(D'*mn+A'*(A*wn-b));
    xn = max(0,min(xnn,1));
    gDxn = gamma*D*xn;
@@ -18,7 +18,7 @@ un = 2*u0;
    for i = 1:p
        gn(i) = max(0,mn(i)+gDwn(i));
        un1(i) = gn(i)-(gDwn(i)-gDxn(i));
-       m = m+(mn(i)-gn(i))*D(i,:);%m规模是（1，N）
+       m = m+(mn(i)-gn(i))*D(i,:);
    end
    zn1 = xn+gamma*m';
    err4 = norm([(zn1-zn)',(un1'-un)'])/norm([zn',un']);
